e622adc406d74c44eb91ff12d88c997acea1662d
kushal69 <30872408+kushal69@users.noreply.github.com>
2017-11-16 08:27:43 +0530

    Initial Commit.

--- src/com/Filter.scala
+++ src/com/Filter.scala
@@ -3,18 +3,21 @@ package com
 object Filter {
   
   def courseMatch(course : String) : String = course match {
-    case "android" => "Android course Price is : 12999"
-    case "big data development " => "Big data development course Price is : 17999"
-    case "spark" => "Spark course Price is : 19999"
-    case _ =>  " Sorry ! , The course " + course + " is not available at Acadgild"
+    case "1" => "Android course Price is : 12999"
+    case "2" => "Big data development course Price is : 17999"
+    case "3" => "Spark course Price is : 19999"
+    case _ =>  "Entered course option is not available at Acadgild"
   }
  
   
   def main(args: Array[String]) : Unit = {
     
-    println("Acadgild course details : \nCourses Available at Acadgild are : \nAndroid \nBig Data Development \nSpark \nPlease Enter your Choice(eg. spark)..." )
-    
-var course = readLine().toString.toLowerCase 
+    println("Courses Available at Acadgild are : ");
+    println("1 - Android");
+    println("2 - Big Data Development");
+    println("3 - Spark");
+    print("Enter the Corresponding course no. : ");
+    var course = readLine().toString.toLowerCase 
     println(courseMatch(course));
     
   }
