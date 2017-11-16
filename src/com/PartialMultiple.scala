9114360f0afbf2f9d75b5431a1d08a58f5b92dec
kushal69 <30872408+kushal69@users.noreply.github.com>
2017-11-16 08:17:22 +0530

    Initial Commit.

--- src/com/PartialMultiple.scala
+++ src/com/PartialMultiple.scala
@@ -1,12 +1,12 @@
 package com
 
 object PartialMultiple {
-  def sum(a: Int, b: Int, c: Int): Int = a + b + c;
+  def sum(n1: Int, n2: Int, n3: Int): Int = n1 + n2 + n3;
   def partsum = sum(4, _: Int, _: Int);
   def sqrNum(f: (Int, Int) => Int): Unit = {
     val ans = f(2, 3);
-    println("Some of sum of three munber is : " + ans);
-    println("Some of sum of Square munber is : " + ans * ans);
+    println("Some of sum of three number is : " + ans);
+    println("Some of sum of Square number is : " + ans * ans);
   }
 
   def main(args: Array[String]): Unit = {
